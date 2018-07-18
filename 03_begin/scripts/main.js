68112ca1a34bc9da34cc966125c0ac8a49ebb024
cvernerie <verneriecamille@gmail.com>
2018-07-18 11:57:48 +0200

    first commit

---
+++ 03_begin/scripts/main.js
@@ -0,0 +1,9 @@
+/*
+ * Ceci est une ardoise JavaScript.
+ *
+ * Saisissez du code JavaScript, puis faites un clic droit ou sélectionnez à partir du menu Exécuter :
+ * 1. Exécuter pour évaluer le texte sélectionné (Cmd-R),
+ * 2. Examiner pour mettre en place un objet Inspector sur le résultat (Cmd-I), ou,
+ * 3. Afficher pour insérer le résultat dans un commentaire après la sélection. (Cmd-L)
+ */
+
